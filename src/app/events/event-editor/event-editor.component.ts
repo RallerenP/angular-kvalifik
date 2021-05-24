d7b50033ae58611a6e1bd2e4d5d41b2c82f414fc
Andreas Møller Laursen <Fyziikme@gmail.com>
2021-05-24 17:10:32 +0200

    connected functionality with database

--- src/app/events/event-editor/event-editor.component.ts
+++ src/app/events/event-editor/event-editor.component.ts
@@ -1,66 +1,94 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 import {EventService} from "../../shared/event.service";
 import {Event} from "../../models/event.model";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-event-editor',
   templateUrl: './event-editor.component.html',
   styleUrls: ['./event-editor.component.scss']
 })
-export class EventEditorComponent implements OnInit {
+export class EventEditorComponent implements OnInit, OnDestroy {
 
   eventForm: FormGroup
-  events: Event[] = this.eventService.getEvents()
+  events: Event[] = []
+  subscription: Subscription
 
   constructor(private eventService: EventService) { }
 
   ngOnInit(): void {
-    if (this.eventService.editMode) {
-      const event = this.events[this.eventService.editItemIndex]
-      this.eventForm = new FormGroup({
-        title: new FormControl(event.name, Validators.required),
-        start: new FormGroup({
-          date: new FormControl('', Validators.required),
-          time: new FormControl('', Validators.required)
-        }),
-        end: new FormGroup({
-          date: new FormControl('', Validators.required),
-          time: new FormControl('', Validators.required)
-        }),
-        description: new FormControl(event.description, Validators.required),
-        eventSchedule: new FormArray([]),
-        photoUrl: new FormControl(event.photoUrl, Validators.required),
-        location: new FormControl(event.location, Validators.required),
-        pinned: new FormControl(event.pinned, Validators.required),
-        responsible: new FormControl(event.responsible, Validators.required),
-        collaboration: new FormControl(event.collaboration, Validators.required)
-      })
-    } else {
-      this.eventForm = new FormGroup({
-        title: new FormControl('', Validators.required),
-        start: new FormGroup({
-          date: new FormControl('', Validators.required),
-          time: new FormControl('', Validators.required)
-        }),
-        end: new FormGroup({
-          date: new FormControl('', Validators.required),
-          time: new FormControl('', Validators.required)
-        }),
-        description: new FormControl('', Validators.required),
-        eventSchedule: new FormArray([]),
-        photoUrl: new FormControl('', Validators.required),
-        location: new FormControl('', Validators.required),
-        pinned: new FormControl('', Validators.required),
-        responsible: new FormControl('', Validators.required),
-        collaboration: new FormControl('', Validators.required)
-      })
-    }
+    this.subscription = this.eventService.get().subscribe( events => {
+      this.events = events
 
+      if (this.eventService.editMode) {
+        const event = this.events[this.eventService.editItemIndex]
+        const startDate = event.startDate.toDate()
+        const endDate = event.endDate.toDate()
+        console.log(`${event.startDate.toDate().getFullYear()}-${event.startDate.toDate().getDate().toString().padStart(2, '0')}-${(event.startDate.toDate().getMonth() + 1).toString().padStart(2, '0')}`)
+        this.eventForm = new FormGroup({
+          title: new FormControl(event.name, Validators.required),
+          start: new FormGroup({
+            date: new FormControl('', Validators.required),
+            time: new FormControl(startDate.getHours().toString().padStart(2, "0") + ":" + startDate.getMinutes().toString().padStart(2, "0"), Validators.required)
+          }),
+          end: new FormGroup({
+            date: new FormControl('', Validators.required),
+            time: new FormControl(endDate.getHours().toString().padStart(2, "0") + ":" + endDate.getMinutes().toString().padStart(2, "0"), Validators.required)
+          }),
+          description: new FormControl(event.description, Validators.required),
+          eventSchedule: new FormArray([]),
+          photoUrl: new FormControl(event.photoUrl, Validators.required),
+          location: new FormControl(event.location, Validators.required),
+          pinned: new FormControl(event.pinned, Validators.required),
+          responsible: new FormControl(event.responsible, Validators.required),
+          collaboration: new FormControl(event.collaboration, Validators.required)
+        })
+      }
+
+    })
+
+    this.eventForm = new FormGroup({
+      title: new FormControl('', Validators.required),
+      start: new FormGroup({
+        date: new FormControl('', Validators.required),
+        time: new FormControl('', Validators.required)
+      }),
+      end: new FormGroup({
+        date: new FormControl('', Validators.required),
+        time: new FormControl('', Validators.required)
+      }),
+      description: new FormControl('', Validators.required),
+      eventSchedule: new FormArray([]),
+      photoUrl: new FormControl('', Validators.required),
+      location: new FormControl('', Validators.required),
+      pinned: new FormControl('', Validators.required),
+      responsible: new FormControl('', Validators.required),
+      collaboration: new FormControl('', Validators.required)
+    })
+
+
+
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe()
+    this.eventService.editMode = false
+  }
+
+  get editMode() {
+    return this.eventService.editMode
   }
 
   onSubmit() {
     this.eventService.addEvent(this.eventForm)
+    this.eventService.editMode = false
+  }
+
+  onEdit() {
+    const event = this.events[this.eventService.editItemIndex]
+    this.eventService.editEvent(this.eventForm, event.id)
+    this.eventService.editMode = false
   }
 
 }
--- src/app/shared/event.service.ts
+++ src/app/shared/event.service.ts
@@ -1,38 +1,56 @@
 import { Injectable } from '@angular/core';
 import {Event} from "../models/event.model";
 import {FormGroup} from "@angular/forms";
-import {Subject} from "rxjs";
+import {Observable, Subject} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {AngularFireAuth} from "@angular/fire/auth";
+import {AngularFirestore} from "@angular/fire/firestore";
+import firebase from "firebase/app";
+import Timestamp = firebase.firestore.Timestamp;
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
 
-  eventsChanged: Subject<Event[]> = new Subject<Event[]>()
-  events: Event[] = []
-  pastEventsChanged: Subject<Event[]> = new Subject<Event[]>()
-  pastEvents: Event[] = []
+  events: Observable<Event[]>
   editMode: boolean = false
   editItemIndex: number = -1
 
-  constructor() { }
 
-  getEvents(): Event[] {
-    return this.events.slice()
+
+  constructor(private http: HttpClient, private afAuth: AngularFireAuth, private afStore: AngularFirestore) {
+    this.events = afStore.collection<Event>('events').valueChanges()
   }
 
-  getPastEvents(): Event[] {
-    return this.pastEvents.slice()
+  get(): Observable<Event[]> {
+    return this.events
   }
 
   addEvent(form: FormGroup) {
+    const event = this.createEvent(form)
+    this.afStore.collection('events').doc(event.id).set(Object.assign({}, event))
+  }
+
+  editEvent(form: FormGroup, id: string) {
+    const event = this.createEvent(form, id)
+    this.afStore.collection('events').doc(id).update(Object.assign({}, event))
+  }
+
+  dateConverter(date: string, time: string): Date {
+    const dateArray = date.split("-")
+    const timeArray = time.split(":")
+    return new Date(+dateArray[0], +dateArray[1] - 1, +dateArray[2], +timeArray[0], +timeArray[1])
+  }
+
+  createEvent(form: FormGroup, id?: string | undefined) {
     const convertedStart = this.dateConverter(form.get('start.date')?.value, form.get('start.time')?.value)
     const convertedEnd =  this.dateConverter(form.get('end.date')?.value, form.get('end.time')?.value)
 
-    const event = new Event(
+    return new Event(
       form.get('title')?.value,
-      convertedStart,
-      convertedEnd,
+      Timestamp.fromDate(convertedStart),
+      Timestamp.fromDate(convertedEnd),
       form.get('description')?.value,
       form.get('eventSchedule')?.value,
       form.get('photoUrl')?.value,
@@ -41,25 +59,9 @@ export class EventService {
       form.get('responsible')?.value,
       form.get('collaboration')?.value,
       'Some room bookings',
-      'published'
+      'published',
+      id
     )
-    this.events.push(event)
-    this.eventsChanged.next(this.events.slice())
-    console.log(this.events)
-    console.log(this.eventsChanged)
-  }
-
-  addPastEvent(index: number, event: Event) {
-    this.pastEvents.push(event)
-    this.events.splice(index, 1)
-    this.eventsChanged.next(this.events.slice())
-    this.pastEventsChanged.next(this.pastEvents.slice())
-  }
-
-  dateConverter(date: string, time: string): Date {
-    const dateArray = date.split("-")
-    const timeArray = time.split(":")
-    return new Date(+dateArray[0], +dateArray[1] - 1, +dateArray[2], +timeArray[0], +timeArray[1])
   }
 
 }
